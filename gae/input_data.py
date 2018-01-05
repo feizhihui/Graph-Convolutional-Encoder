2628585249a81fafd91dec9c1320a3a4b6bee15c
feizhihui <helloworld@csu.edu.cn>
2018-01-05 22:13:04 +0800

    update dev

--- gae/input_data.py
+++ gae/input_data.py
@@ -1,8 +1,68 @@
-import numpy as np
+# encoding:utf-8
 import pickle as pkl
 import networkx as nx
+import numpy as np
+import pandas as pd
 import scipy.sparse as sp
 
+
+def get_matrixM(path):
+    res = {}
+    user_list = []
+    item_list = []
+    with open(path) as f: # B46_154611_chengji.csv
+        line = f.readline()
+        while line:
+            eles = line.split(',')
+            if eles[0] not in res.keys():
+                res[eles[0]] = {}
+                user_list.append(eles[0])
+            if eles[1] not in item_list:
+                item_list.append(eles[1])
+            res[eles[0]][eles[1]] = int(float(eles[2])/10)
+            line = f.readline()
+
+    data=pd.DataFrame(res).T # 成绩矩阵，行表示用户，列表示课程
+    data=pd.DataFrame.fillna(data,0)# 用０填充nan
+    M, N = data.shape
+    '''
+    data=np.array(data)
+    matrix_all=[] # 存储１－１０个级别对应的0/1矩阵
+    for i in range(10):
+        matrix=np.zeros([M,N])
+        matrix_all.append(matrix)
+    for row in range(M):
+        for cow in range(N):
+            if not np.isnan(data[row][cow]):
+                matrix_all[int(data[row][cow]-1)][row][cow]=1  #在对应值的对应级别上修改矩阵
+    '''
+    one_hot_data = np.array([one for one in range(N+M)])  #
+    data_onehot = pd.get_dummies(one_hot_data)
+    return data_onehot,data
+
+def get_adj_01(adj):
+    M, N = adj.shape
+    matrix_all = []  # 存储１－１０个级别对应的0/1矩阵
+    for i in range(10):
+        matrix = np.zeros([M, N])
+        matrix_all.append(matrix)
+    for row in range(M):
+        for cow in range(N):
+            if int(adj[row][cow]) != 0:
+            #if not np.isnan(adj[row][cow]):
+                matrix_all[int(adj[row][cow] - 1)][row][cow] = 1  # 在对应值的对应级别上修改矩阵
+
+    return matrix_all
+
+def get_marix_conbine_matixT(matrix):# 矩阵matrix和其转置矩阵分在处在对角位置组成对称的邻接矩阵
+    matrixT = matrix.T
+    zero_h = np.zeros([matrix.shape[0], matrix.shape[0]])
+    print (zero_h)
+    matrix = np.hstack((zero_h, matrix))
+    zero_v = np.zeros([matrixT.shape[0], matrixT.shape[0]])
+    matrixT = np.hstack((matrixT, zero_v))
+    new = np.vstack((matrix, matrixT))
+    return new
 
 def parse_index_file(filename):
     index = []
@@ -16,17 +76,19 @@ def load_data(dataset):
     names = ['x', 'tx', 'allx', 'graph']
     objects = []
     for i in range(len(names)):
-        objects.append(pkl.load(open("data/ind.{}.{}".format(dataset, names[i]), 'rb', ), encoding='iso-8859-1'))
+        objects.append(pkl.load(open("data/ind.{}.{}".format(dataset, names[i]))))
     x, tx, allx, graph = tuple(objects)
+    # print ('graph:{0}'.format(graph)) {0: [633, 1862, 2582], 1: [2, 652, 654], 2: [1986, 332, 1666, 1, 1454], 3: [2544], 4: ...
+
     test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset))
     test_idx_range = np.sort(test_idx_reorder)
 
     if dataset == 'citeseer':
         # Fix citeseer dataset (there are some isolated nodes in the graph)
         # Find isolated nodes, add them as zero-vecs into the right position
-        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder) + 1)
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
         tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-        tx_extended[test_idx_range - min(test_idx_range), :] = tx
+        tx_extended[test_idx_range-min(test_idx_range), :] = tx
         tx = tx_extended
 
     features = sp.vstack((allx, tx)).tolil()
@@ -34,3 +96,58 @@ def load_data(dataset):
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
 
     return adj, features
+
+
+def getGraph(path):
+    res = {}
+    user_list = []
+    item_list = []
+    with open(path) as f:
+        line = f.readline()
+        while line:
+            eles = line.split(',')
+            if eles[0] not in res.keys():
+                res[eles[0]] = {}
+                user_list.append(eles[0])
+            if eles[1] not in item_list:
+                item_list.append(eles[1])
+            res[eles[0]][eles[1]] = float(eles[2])
+            line = f.readline()
+
+    N = len(user_list) + len(item_list)
+    print(len(user_list))
+    print(len(item_list))
+    i = 0
+    id_map = dict()
+    for user in user_list:
+        id_map[user] = i
+        i = i + 1
+    for item in item_list:
+        id_map[item] = i
+        i = i + 1
+
+    adj = np.zeros([N, N])
+    for key_user in res:
+        for key_item in res[key_user]:
+            adj[id_map[key_user ]][id_map[key_item ]]=res[key_user][key_item]
+            adj[id_map[key_item ]][id_map[key_user]] = res[key_user][key_item]
+
+    one_hot_data=np.array([one for one in range(N)])  #
+    data_onehot=pd.get_dummies(one_hot_data)
+    return adj,data_onehot
+def getFeature():
+    # student_kch_feature_p_350.txt student_kch_feature_q_350.txt
+    feature_user = pd.read_csv('./data/student_kch_feature_p_350.txt', sep=',', header=None)
+    feature_item = pd.read_csv('./data/student_kch_feature_q_350.txt', sep=',', header=None)
+
+    feature_item = feature_item.T
+    feature_x = np.vstack((feature_user, feature_item))
+    print('user shape:{0},item shape:{1}'.format(feature_user.shape, feature_item.shape))
+    print('feature_x shape:{0}'.format(feature_x.shape))
+    return feature_x
+if __name__=='__main__':
+    print()
+    # data, adj = getData('./data/B46_154611_chengji.csv')
+    #adj = sp.coo_matrix(adj)
+    #print(adj.shape)
+
--- gae/train.py
+++ gae/train.py
@@ -1,23 +1,23 @@
+# encoding:utf-8
 from __future__ import division
 from __future__ import print_function
 
-import time
 import os
+import time
+
+# Train on CPU (hide GPU) due to memory constraints
+os.environ['CUDA_VISIBLE_DEVICES'] = ""
 
 import tensorflow as tf
 import numpy as np
 import scipy.sparse as sp
 
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import confusion_matrix
 
-# Train on CPU (hide GPU) due to memory constraints
-os.environ['CUDA_VISIBLE_DEVICES'] = "3"
-
-from gae.optimizer import OptimizerAE, OptimizerVAE
-from gae.input_data import load_data
-from gae.model import GCNModelAE, GCNModelVAE
-from gae.preprocessing import preprocess_graph, construct_feed_dict, sparse_to_tuple, mask_test_edges
+from optimizer import OptimizerAE, OptimizerVAE
+from input_data import get_adj_01, get_matrixM, get_marix_conbine_matixT
+from model import GCNModelAE, GCNModelVAE
+from preprocessing import preprocess_graph, construct_feed_dict, sparse_to_tuple, mask_test_edges
 
 # Settings
 flags = tf.app.flags
@@ -30,42 +30,52 @@ flags.DEFINE_float('weight_decay', 0., 'Weight for L2 loss on embedding matrix.'
 flags.DEFINE_float('dropout', 0., 'Dropout rate (1 - keep probability).')
 
 flags.DEFINE_string('model', 'gcn_ae', 'Model string.')
-flags.DEFINE_string('dataset', 'cora', 'Dataset string.')
+flags.DEFINE_string('dataset', 'citeseer', 'Dataset string.')
 flags.DEFINE_integer('features', 1, 'Whether to use features (1) or not (0).')
 
 model_str = FLAGS.model
 dataset_str = FLAGS.dataset
 
-# Load data adj:(2708,2708) 10556 non-zeros features:(2708,1433)
-adj, features = load_data(dataset_str)
+# Load data
+# adj, features = load_data(dataset_str)
 
+data_onehot, adj = get_matrixM('./data/B46_154611_chengji.csv')
+adj = get_marix_conbine_matixT(adj)
+adj = sp.csr_matrix(adj)
+
+# features=getFeature()# F-feature
+features = data_onehot  # one-hot feature
+features = sp.lil_matrix(features)
+# 3848
 # Store original adjacency matrix (without diagonal entries) for later
 adj_orig = adj
 adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
 adj_orig.eliminate_zeros()
-# 8976,4488 263 263  527  527  undirect graph
+# 3272  1636  96  96   192 192
 adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_test_edges(adj)
+adj_Rs = get_adj_01(adj_train.toarray())
 adj = adj_train
+adj_R = [sp.csr_matrix(adj_one) for adj_one in adj_Rs]
 
 if FLAGS.features == 0:
     features = sp.identity(features.shape[0])  # featureless
 
 # Some preprocessing
-adj_norm = preprocess_graph(adj)  # coords, values, shape
+adj_R_norm = [preprocess_graph(one_adj) for one_adj in adj_R]
 
 # Define placeholders
 placeholders = {
     'features': tf.sparse_placeholder(tf.float32),
-    'adj': tf.sparse_placeholder(tf.float32),
+    'adj': [tf.sparse_placeholder(tf.float32) for _ in range(10)],
     'adj_orig': tf.sparse_placeholder(tf.float32),
     'dropout': tf.placeholder_with_default(0., shape=())
 }
 
 num_nodes = adj.shape[0]
 
-features = sparse_to_tuple(features.tocoo())  # coords, values, shape
+features = sparse_to_tuple(features.tocoo())
 num_features = features[2][1]
-features_nonzero = features[1].shape[0]  # use to droput
+features_nonzero = features[1].shape[0]
 
 # Create model
 model = None
@@ -73,7 +83,7 @@ if model_str == 'gcn_ae':
     model = GCNModelAE(placeholders, num_features, features_nonzero)
 elif model_str == 'gcn_vae':
     model = GCNModelVAE(placeholders, num_features, num_nodes, features_nonzero)
-# 815.985  0.5006
+
 pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()
 norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
 
@@ -101,6 +111,26 @@ cost_val = []
 acc_val = []
 
 
+# 给定混淆矩阵M，计算准确率和召回率
+def get_precision_recall_score_confusion(M):
+    # preds_all = [2, 2, 4, 3, 4]
+    # pre_sore=auc(labels_all,preds_all)
+    # M = confusion_matrix(labels_all, preds_all)
+    print(M)
+    n = len(M)
+    precision = []
+    recall = []
+    for i in range(len(M[0])):
+        rowsum, colsum = sum(M[i]), sum(M[r][i] for r in range(n))
+        try:
+            precision.append(M[i][i] / float(colsum))
+            recall.append(M[i][i] / float(rowsum))
+        except ZeroDivisionError:
+            precision.append(0)
+            recall.append(0)
+    return np.mean([i for i in precision if not np.isnan(i)]), np.mean([i for i in recall if not np.isnan(i)])
+
+
 def get_roc_score(edges_pos, edges_neg, emb=None):
     if emb is None:
         feed_dict.update({placeholders['dropout']: 0})
@@ -114,35 +144,51 @@ def get_roc_score(edges_pos, edges_neg, emb=None):
     preds = []
     pos = []
     for e in edges_pos:
-        preds.append(sigmoid(adj_rec[e[0], e[1]]))
-        pos.append(adj_orig[e[0], e[1]])
+        if not np.isnan(adj_orig[e[0], e[1]]):
+            preds.append(sigmoid(adj_rec[e[0], e[1]]))
+            pos.append(adj_orig[e[0], e[1]])
 
     preds_neg = []
     neg = []
     for e in edges_neg:
-        preds_neg.append(sigmoid(adj_rec[e[0], e[1]]))
-        neg.append(adj_orig[e[0], e[1]])
+        if not np.isnan(adj_orig[e[0], e[1]]):
+            preds_neg.append(sigmoid(adj_rec[e[0], e[1]]))
+            neg.append(adj_orig[e[0], e[1]])
 
     preds_all = np.hstack([preds, preds_neg])
-    labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds))])
-    roc_score = roc_auc_score(labels_all, preds_all)
-    ap_score = average_precision_score(labels_all, preds_all)
+    # labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds))])
+    # labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))]) # 当前是多分类问题评估
+    labels_all = np.hstack([pos, neg])
+    '''
+    ncorrects = sum(predictions == labels)
+    accuracy = 100 * sklearn.metrics.accuracy_score(labels, predictions)
+    f1 = 100 * sklearn.metrics.f1_score(labels, predictions, average='weighted')
+    '''
 
-    return roc_score, ap_score
+    # roc_score = roc_auc_score(labels_all, preds_all)
+    # ap_score = average_precision_score(labels_all, preds_all)
+    preds_all = [round(one * 10) for one in preds_all]
+    confusion = confusion_matrix(labels_all, preds_all)
+
+    precision, recall = get_precision_recall_score_confusion(confusion)
+    # print('====',con_score)
+
+    # return roc_score, ap_score
+    return precision, recall
 
 
 cost_val = []
 acc_val = []
 val_roc_score = []
-# 8976+2708
-adj_label = adj_train + sp.eye(adj_train.shape[0])  # adj_train + I
-adj_label = sparse_to_tuple(adj_label)  # coords, values, shape
+
+adj_label = adj_train + sp.eye(adj_train.shape[0])
+adj_label = sparse_to_tuple(adj_label)
 
 # Train model
 for epoch in range(FLAGS.epochs):
     t = time.time()
-    # Construct feed dictionary
-    feed_dict = construct_feed_dict(adj_norm, adj_label, features, placeholders)
+    # Construct feed dictionary  adj_orig
+    feed_dict = construct_feed_dict(adj_R_norm, adj_label, features, placeholders)
     feed_dict.update({placeholders['dropout']: FLAGS.dropout})
     # Run single weight update
     outs = sess.run([opt.opt_op, opt.cost, opt.accuracy], feed_dict=feed_dict)
